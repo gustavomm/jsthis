7f920d8b3235b376a170ab524f649ad74aeea813
gustavomm <gustavomm22.gm@gmail.com>
2019-10-08 01:05:56 -0300

    initial layout, some animations and transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import { HighlightResult } from 'ngx-highlightjs';
+import Typed from 'typed.js';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'jsthis';
+    response: HighlightResult;
+
+    code = `    function myFunction() {
+        document.getElementById("demo1").innerHTML = "Hello there!";
+        document.getElementById("demo2").innerHTML = "How are you?";
+    }`
+    typed = {};
+
+    onHighlight(e) {
+        this.response = {
+            language: e.language,
+            r: e.r,
+            second_best: '{...}',
+            top: '{...}',
+            value: '{...}'
+        }
+      }
+    
+      ngOnInit() {
+        var typed = new Typed('.typed', {
+            strings: ['jsThis', 'jsThis', 'jsThis', 'made by Gustavo'],
+            typeSpeed: 100,
+            backSpeed: 30,
+            backDelay: 4000,
+            smartBackspace: false,
+            loop: true,
+            autoInsertCss: true
+          });
+    }
 }
